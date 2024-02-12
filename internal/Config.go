b55346d6afb4f54974d0df0facb2215fac7e21ff
Aleksey Kalinovski <aleksey.kalinovski@gmail.com>
2024-02-11 19:38:03 -0500

    Config sytem

---
+++ internal/Config.go
@@ -0,0 +1,24 @@
+package internal
+
+import (
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	clientID    string `yaml:"clientID"`
+	redirectURI string `yaml:"redirectURI"`
+}
+
+func ParseYamlConfig(cfg *Config, configPath string) error {
+	f, err := os.Open(configPath)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	decoder := yaml.NewDecoder(f)
+	err = decoder.Decode(cfg)
+	return err
+}
